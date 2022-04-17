32b25e113aa8f5ba08ab432d5f4af8edaf5ae725
Rachmat Purwa Saputra <rachmatpurwa393@gmail.com>
2022-04-17 21:35:29 +0700

    first attempt to commit

---
+++ src/ItemList/Koneksi.java
@@ -0,0 +1,31 @@
+package ItemList;
+
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author PY7
+ */
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+   
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi == null){
+            try {
+                String url = "jdbc:mysql://localhost:3306/crudmysql";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Berhasil Terhubung");
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String args[]){
+        getKoneksi();
+    }  
+}
